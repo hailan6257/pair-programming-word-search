5738ec6dff78e3f31ebbb215d6adc9675b3cfa20
Hailan Wang <hailan6257@email.com>
2021-12-01 18:51:24 -0500

    Add wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const transpose = function(matrix) {
+    let result = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let col = [];
+      for (let j = 0; j < matrix.length; j++) {
+        col.push(matrix[j][i]);
+      }
+      result.push(col);
     }
-}
+    return result;
+  };
+  const matrix = transpose(letters);//transformed martrix
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = matrix.map(ls => ls.join(''));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
